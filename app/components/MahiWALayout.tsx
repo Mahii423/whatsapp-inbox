c1e445ecd3a4f2929f36eedaf4bb2fe087e94694
Aamir <aaaamirmahii@gmail.com>
2026-09-01 08:17:07 +0500

    fix chat start after 220px sidebar

--- app/components/MahiWALayout.tsx
+++ app/components/MahiWALayout.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Sidebar from "./Sidebar";
+
 export default function MahiWALayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="min-h-screen bg-[#f8f9fb]">
       <Sidebar />
-      <main className="md:ml-">
+      <main className="md:ml- w-full md:w-[calc(100%-220px)] min-h-screen">
         {children}
       </main>
     </div>
