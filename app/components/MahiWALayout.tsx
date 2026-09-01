b862f194894ebc3a6977a03a99e21327df3775ef
Aamir <aaaamirmahii@gmail.com>
2026-09-01 08:40:09 +0500

    force inline style 220px ml

--- app/components/MahiWALayout.tsx
+++ app/components/MahiWALayout.tsx
@@ -5,7 +5,7 @@ export default function MahiWALayout({ children }: { children: React.ReactNode }
   return (
     <div className="min-h-screen bg-[#f8f9fb]">
       <Sidebar />
-      <main className="md:ml- w-full md:w-[calc(100%-220px)] min-h-screen">
+      <main style={{ marginLeft: '220px', width: 'calc(100% - 220px)' }} className="min-h-screen">
         {children}
       </main>
     </div>
